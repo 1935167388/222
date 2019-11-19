8a20edf68f200a5142626db2a7964bd27ed63f1a
1935167388 <1935167388@qq.com>
2019-11-19 10:26:38 +0800

    First commit.

---
+++ app/src/main/java/com/example/lx2/AnotherActivity.java
@@ -0,0 +1,42 @@
+package com.example.lx2;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+        Intent intent=getIntent();
+        String name=intent.getStringExtra("name");
+        Integer age=intent.getIntExtra("age", 20);
+        Toast.makeText(this,name+" "+age,Toast.LENGTH_LONG).show();
+
+        Button button=(Button)findViewById(R.id.fanhui);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=getIntent();
+                String name=intent.getStringExtra("name");
+                Integer age=intent.getIntExtra("age", 20);
+                intent.putExtra("result","姓名:"+name+" 年龄"+age);
+                setResult(0,intent);
+                finish();
+            }
+        });
+    }
+
+}
